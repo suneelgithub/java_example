05ae608ea71769547e996a4a2322e73e10460a30
Sonaker <Suneelkumar.Sonaker@CenturyLink.com>
2017-07-19 18:57:00 +0530

    first commit

---
+++ src/com/suneel/test/Test1.java
@@ -0,0 +1,15 @@
+package com.suneel.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String str = "suneel";
+		String str2 =str.concat(" kumar");
+		int length = str.length();
+		System.out.println(str + " " + length);
+		
+		System.out.println(str2 + " " + str2.length());
+
+	}
+}
